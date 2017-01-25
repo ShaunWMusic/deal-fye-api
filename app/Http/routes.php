8a49113c04f023d8c4d7ea3fc83ea0fc313258fd
“Shaun <shaunwmusic@gmail.com>
2017-01-25 12:45:30 -0600

    Fix routes to get page up and running

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -12,9 +12,10 @@ use App\Notifications\LessonPublished;
 |
 */
 Route::get('/', function () {
-  $user = App\User::first();
-  $lesson = App\Lesson::first();
-  $user->notify(new LessonPublished($lesson));
+  // $user = App\User::first();
+  // $lesson = App\Lesson::first();
+  // $user->notify(new LessonPublished($lesson));
+  return view('welcome');
 });
 
 
